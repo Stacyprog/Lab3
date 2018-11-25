26db94491d3b0e9b0134cdaeaf8cbfb81ae9db30
Katerina Efimova <efekma@gmail.com>
2018-11-25 23:52:39 +0300

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,79 +1,42 @@
-
-
-public class Person extends Habitat implements Acting, Speech{
+public class Animal extends Habitat implements Speech, Acting {
     Forest place;
+    Person person;
+    public String type;
 
-    Person(String name) {
+    Animal(String name) {
         super.name = name;
     }
 
     public void move(Forest place) {
         this.place = place;
+        place.setNoise();
         switch (this.place) {
             case FIELD: {
-                System.out.println(name + "пришел к друзьям на поле");
+                System.out.println(name + " находится на поле");
                 break;
             }
             case REEDS: {
-                System.out.println(name + "полез в камыши искать Тигрулю");
+                System.out.println(name + " вышел из камышей в поле");
                 break;
             }
-            case PATH: {
-                System.out.println(name + " идет по дорожке");
+            case PATH:
+            {
+                System.out.println(name + " пошел домой по дорожке");
                 break;
             }
-            case BRIDGE: {
-                System.out.println(name + " подошел к мосту");
+            case BRIDGE:
+            {
+                System.out.println(name + " стоит на мосту и ждет Кристофера");
             }
         }
-    }
-
-    void mood() {
-        System.out.println("Настроение у него - солнечное и безоблачное");
-    }
-
-    void think() {
-        switch (place) {
-            case PATH: {
-                System.out.print("\"Если я встану на нижнюю перекладину перил моста и наклонюсь над рекой, ");
-                System.out.print("я узнаю все-все на свете и тогда я расскажу все это Пуху, который пока ");
-                System.out.println("еще знает не все на свете,\" - подумал " + name);
-                break;
-            }
-            case BRIDGE: {
-                if (getKnowledge().equals("Знает всё на свете")) {
-                    System.out.println("Я теперь знаю всё-всё на свете, и я должен рассказать об этом Пуху!");
-                } else {
-                    System.out.print(getKnowledge() + " - подумал " + name + ", увидев своих друзей.");
-                }
-                break;
-            }
-            default: {
-                System.out.println("никаких планов на день. - подумал " + name);
-                break;
-            }
-        }
-        know();
-    }
+     }
 
     public void ask(String a) {
-        System.out.println(name + " спрашивает " + a);
+        System.out.println(name + " спрашивает " + a + ": \"Как тебя зовут?\"");
     }
 
     public void answer() {
-        System.out.println(name + " отвечает");
-    }
-
-    @Override
-    public void know() {
-        setKnowledge("Знает 2х19");
-        if ((place.equals(Forest.BRIDGE)) && (Forest.FIELD.getNoise())) {
-            if (!place.getNoise()) {
-                setKnowledge("Знает всё-всё на свете");
-            } else {
-                setKnowledge("Сегодня день, когда нужно что-то сделать.");
-            }
-        }
+        System.out.println(name + " отвечает: \"Меня зовут...\"");
     }
 
     @Override
@@ -83,11 +46,12 @@ public class Person extends Habitat implements Acting, Speech{
 
     @Override
     public String toString() {
-        return "Person [x = " + name + "]";
+        return "Animals [x = " + name +  "]";
     }
 
     @Override
     public int hashCode() {
         return super.hashCode();
     }
+
 }
