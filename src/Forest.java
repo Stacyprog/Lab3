7deeea000b7444c1a7142dfbf83c651add2bac1d
Anastasia <anastasia.d.sokolova@gmail.com>
2018-11-25 23:05:40 +0300

    test1.2

--- src/Forest.java
+++ src/Forest.java
@@ -4,5 +4,12 @@ public enum   Forest {
        FIELD,
        REEDS,
        PATH,
-       BRIDGE
+       BRIDGE ;
+    private boolean noise;
+    public void setNoise(boolean noise) {
+        this.noise = noise;
+    }
+    public boolean getNoise() {
+        return noise;
+    }
 }
