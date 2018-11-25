ec04929f96f70f104de4250394850bd2513041d6
Katerina Efimova <efekma@gmail.com>
2018-11-25 23:53:20 +0300

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,6 @@
-public class Person extends Habitat implements Acting {
+public class Person extends Habitat implements Acting, Speech{
     Forest place;
+
     Person(String name) {
         super.name = name;
     }
@@ -26,25 +27,49 @@ public class Person extends Habitat implements Acting {
     }
 
     void mood() {
-        System.out.println("Настроение : солнечное и безоблачное");
+        System.out.println("Настроение у него - солнечное и безоблачное");
     }
 
     void think() {
         switch (place) {
             case PATH: {
-                System.out.print("Если я встану на нижнюю перекладину перил моста и наклонюсь над рекой, ");
+                System.out.print("\"Если я встану на нижнюю перекладину перил моста и наклонюсь над рекой, ");
                 System.out.print("я узнаю все-все на свете и тогда я расскажу все это Пуху, который пока ");
-                System.out.println("еще знает не все на свете. - подумал " + name);
+                System.out.println("еще знает не все на свете,\" - подумал " + name);
                 break;
             }
             case BRIDGE: {
-                System.out.print("Сегодня день, когда нужно что-то сделать.");
-                System.out.println(" - подумал " + name + ", увидев своих друзей.");
+                if (getKnowledge().equals("Знает всё на свете")) {
+                    System.out.println("Я теперь знаю всё-всё на свете, и я должен рассказать об этом Пуху!");
+                } else {
+                    System.out.print(getKnowledge() + " - подумал " + name + ", увидев своих друзей.");
+                }
                 break;
             }
             default: {
                 System.out.println("никаких планов на день. - подумал " + name);
                 break;
+            }
+        }
+        know();
+    }
+
+    public void ask(String a) {
+        System.out.println(name + " спрашивает " + a);
+    }
+
+    public void answer() {
+        System.out.println(name + " отвечает");
+    }
+
+    @Override
+    public void know() {
+        setKnowledge("Знает 2х19");
+        if ((place.equals(Forest.BRIDGE)) && (Forest.FIELD.getNoise())) {
+            if (!place.getNoise()) {
+                setKnowledge("Знает всё-всё на свете");
+            } else {
+                setKnowledge("Сегодня день, когда нужно что-то сделать.");
             }
         }
     }
